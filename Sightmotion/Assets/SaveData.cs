b9244524faa1f70505c9d8f08979267fbbb95522
Vilhelm Norström <Vilhelmnorst@gmail.com>
2024-12-19 11:01:18 +0100

    Add saving

---
+++ Sightmotion/Assets/SaveData.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class SaveData : MonoBehaviour
+{
+    public gameObject posVec;
+    private TargetHandler handlerScript;
+
+
+
+    private string positionDataFilePath; // ändra till rätt data
+    public GameObject Head; 
+    public GameObject Stylus;
+    private List<string> positionData;
+
+    void Start()
+    {
+
+        handlerScript = posVec.GetComponent<TargetHandler>();
+        positionData = new List<string>();
+        positionDataFilePath = Path.Combine(Application.persistentDataPath, "TEST.csv");
+        if (File.Exists(positionDataFilePath)){
+            File.Delete()
+        }
+
+
+        // Write the header line to the position data file
+        File.WriteAllText(positionDataFilePath, "TimeStamp, PosX, PosY, PosZ\n"); // ändra till rätt grejer
+    }
+
+    void Update()
+    {
+        Vector3 currentTrackerPosition = handlerScript.resultPoint
+
+        // Format the current positions into a string for the CSV file
+        string positionDataLine = string.Format(
+            "{0},{1},{2},{3}",
+            DateTime.Now,
+            currentTrackerPosition.x, currentTrackerPosition.y, currentTrackerPosition.z,
+        );
+
+        // Store the formatted string in the list
+        positionData.Add(positionDataLine);
+    }
+
+    public void SavePositionData()
+    {
+        Debug.Log(positionDataFilePath);
+        // Write the list of position data lines to the CSV file after each round
+        File.AppendAllLines(positionDataFilePath, positionData);
+
+        // Clear the list after saving
+        positionData.Clear();
+    }
+}
